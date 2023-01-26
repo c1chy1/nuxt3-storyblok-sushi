f66b311d619678094c06f2a04399e1173bb70322
c1chy1 <cichy08081987pp@gmail.com>
2023-01-26 23:38:04 +0100

    config sushi #1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,20 @@
-import {apiPlugin} from "@storyblok/vue"
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+
+import { apiPlugin } from '@storyblok/vue'
+
+
 
 export default defineNuxtConfig({
-    ssr: false,
+
     css: ["~/assets/css/tailwind.css"],
-    build: {
-        transpile: ['@marvr/storyblok-rich-text-vue-renderer'],
-    },
-    image: {
-        storyblok: {
-            baseURL: 'https://a.storyblok.com',
-        },
-    },
-    modules: [['@storyblok/nuxt',
-        {
-            accessToken: process.env.STORYBLOK_API_TOKEN,
+    modules:[ ['@storyblok/nuxt',
+        { accessToken: process.env.STORYBLOK_API_TOKEN,
             bridge: true,
-            use: [apiPlugin]
-        }],
+             use: [apiPlugin]
+        }] ,
 
-        '@nuxt/image-edge',
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/fontaine',
-        '@nuxtjs/critters'],
+
+        '@nuxtjs/tailwindcss' ,'@nuxt/image-edge' ],
+
 })
-
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,24 @@
-<script setup lang="ts">
-const story = await useStoryblok('home', { version: 'draft' })
-</script>
+
 <template>
-  <main role="main">
-    <StoryblokComponent v-if="story" :blok="story.content" />
-  </main>
+
+  <StoryblokComponent v-if="story" :blok="story.content" class="px-10" />
+
 </template>
+
+
+<script setup lang="ts">
+
+
+
+const story = ref(null);
+onMounted(async () => {
+  const storyblokApi = useStoryblokApi();
+  const { data } = await storyblokApi.get("cdn/stories/Home", {
+    version: 'draft',
+
+  });
+  story.value = data.story;
+
+  console.log(story)
+});
+</script>

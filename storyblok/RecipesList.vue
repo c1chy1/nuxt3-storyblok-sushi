29194cc0faa22030deb18205d34aac2b9dbe105a
c1chy <paul@daro.de>
2023-01-29 17:10:19 +0100

    fix RecipesList Layout  #1

--- storyblok/RecipesList.vue
+++ storyblok/RecipesList.vue
@@ -10,30 +10,18 @@ await fetchRecipes()
 </script>
 <template>
   <section class="recipes-list w-full">
-    <div class="container mx-auto py-20">
-      <h2 class="text-shrimp-500 font-display text-4xl font-bold mb-8 w-2/3">
+    <div class="container mx-auto py-20 px-8 sm:px-0">
+      <h2 class="text-shrimp-500 font-display text-4xl font-bold mb-16 w-2/3">
         {{ blok.title }}
       </h2>
-      <div class="grid grid-cols-3 gap-12">
-        <article
-            v-for="{ uuid, content, slug } of recipes"
-            class="bg-white rounded-lg overflow-hidden shadow-md"
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12">
+        <RecipeCard
+            v-for="{ uuid, content, slug } in recipes"
             :key="uuid"
-        >
-          <img class="w-full"
-
-               :src="content.media.filename"
-               :alt="content.media.alt" />
-          <div class="p-4 relative min-h-[200px]">
-            <h3 class="font-bold text-xl text-shrimp-600 w-1/2">
-              <NuxtLink :to="`/recipes/${slug}`"> {{ content.title }}</NuxtLink>
-            </h3>
-            <span class="bg-shrimp-100 rounded-full absolute right-8 -top-[33px] p-4">
-              <img class="w-[36px] h-[36px]"
-                   :src="content.category.content.icon.filename" alt="" />
-            </span>
-          </div>
-        </article>
+            :uuid="uuid"
+            :content="content"
+            :slug="slug"
+        />
       </div>
     </div>
   </section>

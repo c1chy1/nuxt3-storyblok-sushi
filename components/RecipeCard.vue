0f8dc898ba7bde38bdfd61ff3a259ff5a11f2474
c1chy <paul@daro.de>
2023-01-29 16:22:48 +0100

    image size fix  #1

--- components/RecipeCard.vue
+++ components/RecipeCard.vue
@@ -12,13 +12,13 @@ defineProps<{
                 provider="storyblok"
                 format="webp"
                 sizes="sm:100vw md:75vw lg:600px xl:1200px" class="w-full aspect-video object-cover" :src="content.media.filename" :alt="content.media.alt" />
-      <div class="p-4 relative min-h-200px">
+      <div class="p-4 relative min-h-[200px]">
         <h3 class="font-bold text-xl text-shrimp-600 w-1/2">
           {{ content.title }}
         </h3>
-        <span class="bg-gray-100 rounded-full absolute right-8 -top-33px p-4">
+        <span class="bg-gray-100 rounded-full absolute right-8 -top-[33px] p-4">
           <img
-              class="w-36px h-36px filter-grayscale contrast-25"
+              class="w-[36px] h-[36px] filter-grayscale contrast-25"
               :src="content.category.content.icon.filename"
               alt=""
           />

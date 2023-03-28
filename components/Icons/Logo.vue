2de095be2cd9a36c7a68242d9947c2da4f48b867
c1chy1 <cichy08081987pp@gmail.com>
2023-03-28 16:23:59 +0200

    fix   #done

--- components/Icons/Logo.vue
+++ components/Icons/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class=" px-4 lg:px-8 container mx-auto flex flex-col z-50">
 
-  <NuxtLink :to="localeUrl() || '/home'"
+  <NuxtLink :to="localeUrl() || '/'"
             class=" logo pt-4  flex items-center font-display text-xl lg:text-3xl font-bold text-gray-800 dark:text-white transition-all">
     <img src="/logo.svg" class="h-10 w-10 mr-2" alt="Sushi Wuut"/>
     c1chy<span class="text-shrimp-600">Sushi</span>

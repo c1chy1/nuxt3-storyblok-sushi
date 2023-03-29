ef4884c3db2a09b474ea304030a42d7df2530f57
c1chy1 <cichy08081987pp@gmail.com>
2023-03-29 18:13:07 +0200

    image-edge   #1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -112,9 +112,6 @@ export default defineNuxtConfig({
     },
     image: {
 
-        provider: 'netlify',
-        screens: { xs: 320, sm: 640, md: 768, lg: 1024, xl: 1280, xxl: 1536, '2xl': 1536 },
-
         storyblok: {
             baseURL: 'https://a.storyblok.com',
         },

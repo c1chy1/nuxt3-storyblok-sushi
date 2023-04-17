e0c988c88a8e9feffc70b0e5d74aafab6ea74b54
c1chy1 <cichy08081987pp@gmail.com>
2023-04-17 16:57:23 +0200

    mobil hero fix #done

--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -1,7 +1,7 @@
 <template>
 
-  <div id="hero" class="hero container mx-auto flex flex-col  px-2  sm:px-4 md:px-8  lg:px-12 xl:px-16 2xl:px-28">
-    <div class="flex pt-8 lg:pt-12 justify-between flex-col-reverse sm:flex-row mx-auto  font-sans">
+  <div id="hero" class="hero container h-screen sm:h-auto mx-auto flex flex-col  px-2  sm:px-4 md:px-8  lg:px-12 xl:px-16 2xl:px-28">
+    <div class="flex justify-between  xl:h-32  xl:pt-12 flex-col-reverse sm:flex-row mx-auto  font-sans">
 
       <div class=" w-full sm:w-1/2">
         <NuxtImg
@@ -10,8 +10,10 @@
             provider="storyblok"
             format="webp"
             quality="100"
-            sizes="sm:75vw lg:50vw  "
-            class=" lg:w-full h-full mx-auto"
+            width="500"
+            height="500"
+            sizes="sm:150px lg:250px  xl:500px "
+            class=" mx-auto"
             :src="blok.media.filename"
             :alt="blok.media.alt"
             :title="blok.media.title"
